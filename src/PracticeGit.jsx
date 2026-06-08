b562a6caccc79b0e4efff77743fbb65c185d12cf
gudari <gudari183@gmail.com>
2026-06-09 01:44:46 +0530

    Main second Commit

--- src/PracticeGit.jsx
+++ src/PracticeGit.jsx
@@ -3,6 +3,7 @@ export function PracticeGit() {
     <div>
       <h1>Practice Git</h1>
       <p>This is a practice component for Git.</p>
+      <p>This is 2nd commit on main branch.</p>
     </div>
   );
 }
