8abc30af3aee4055d2d2e11a7f95a513a91a2d2d
gudari <gudari183@gmail.com>
2026-06-09 01:46:33 +0530

    First commit in feature branch.

--- src/PracticeGit.jsx
+++ src/PracticeGit.jsx
@@ -4,6 +4,8 @@ export function PracticeGit() {
       <h1>Practice Git</h1>
       <p>This is a practice component for Git.</p>
       <p>This is 2nd commit on main branch.</p>
+      <p>First commit in feature branch.</p>
+      
     </div>
   );
 }
