105a6952a9f329c588a5849de5eb3ec31f1902a4
gudari <gudari183@gmail.com>
2026-06-10 01:28:45 +0530

    third commit on main branch

--- src/PracticeGit.jsx
+++ src/PracticeGit.jsx
@@ -4,6 +4,9 @@ export function PracticeGit() {
       <h1>Practice Git</h1>
       <p>This is a practice component for Git.</p>
       <p>This is 2nd commit on main branch.</p>
+      <p>3rd commit in main.</p>
+     
+       
     </div>
   );
 }
